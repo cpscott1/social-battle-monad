5768cb84f12e97115cbbe742b0fe1d43bf04df65
cperry <>
2025-02-21 17:42:50 -0600

    chore: update network config to monad testnet and improve CI workflow

--- packages/nextjs/utils/customChains.ts
+++ packages/nextjs/utils/customChains.ts
@@ -1,21 +1,29 @@
-import { defineChain } from "viem";
+import { defineChain, http } from "viem";
 
 // TODO: Add Chain details here.
-export const monadDevnet = defineChain({
-  id: 41454,
-  name: "Monad Devnet",
-  nativeCurrency: { name: "Monad", symbol: "MON", decimals: 18 },
+export const monadTestnet = defineChain({
+  id: 10143,
+  name: "Monad Testnet",
+  nativeCurrency: { name: "Monad Testnet", symbol: "MON", decimals: 18 },
+  iconUrl: "https://miro.medium.com/v2/resize:fit:400/0*aRHYdVg5kllfc7Gn.jpg",
   rpcUrls: {
     default: {
-      // TODO: Add Monad RPC URL
-      http: ["<MONAD_RPC_URL>"],
+      http: ["https://testnet-rpc.monad.xyz/"],
+    },
+    public: {
+      http: ["https://testnet-rpc.monad.xyz/"],
     },
   },
   blockExplorers: {
     default: {
-      name: "Monad Devnet Blockscout",
-      // TODO: Add Explorer URL
-      url: "<MONAD_BLOCKSCOUT_URL>",
+      name: "Monad Testnet",
+      url: "https://monad-testnet.socialscan.io/",
     },
   },
+  testnet: true,
 });
+
+export const chainArray = [monadTestnet];
+export const transportsObject = {
+  [monadTestnet.id]: http(),
+};
